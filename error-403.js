1e39e284f09db9505cb83c5f0714269aa4778e9e
noasman971 <noah.kichenassamy@edu.esiee-it.fr>
2025-08-05 23:12:50 +0200

    Add forbiden function

---
+++ error-403.js
@@ -0,0 +1,14 @@
+export function error403(req, res)
+{
+    if (req.headers['api-key'] !== process.env.API_KEY) {
+        return res.status(403).send(`
+            <html lang="fr">
+              <head><title>Accès refusé</title></head>
+              <body style="font-family:sans-serif;text-align:center;padding-top:50px;">
+                <h1>403 - Accès refusé</h1>
+                <p>Tu n’as pas le droit d’être ici 🛑</p>
+              </body>
+            </html>
+        `);
+    }
+}
--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors');
+const {error403} = require("./error-403");
+require('dotenv').config();
+
+app.use(cors({
+    origin: 'http://localhost:8081',
+    method: ['*']
+}));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+
+    error403(req, res)
+
+    res.send('Hello World!');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
